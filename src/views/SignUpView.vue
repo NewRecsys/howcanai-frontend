0e051dc384c44623f908c7c1030d2c02dd69f901
gangjoohyeong <bles@kakao.com>
2023-07-20 22:28:21 +0900

    update: sign up style

--- src/views/SignUpView.vue
+++ src/views/SignUpView.vue
@@ -1,32 +1,23 @@
 <template>
   <div class="signup-container">
-    <div class="input-title" style="width: 100%; border-bottom: 1px solid rgba(75, 75, 70, 0.60);; ">
-      <img src="../assets/sideLogo.svg" style="padding: 14px;">
-    </div>
-    <!-- <h1>회원가입</h1> -->
-    <form @submit.prevent="submitForm" style="color: white;">
-      <div class="input-container" style="padding-top: 20px;">
-        <!-- <label for="username">username:</label> -->
-        <input type="text" placeholder="username" id="username" v-model="username" required>
-      </div>
-      <div class="input-container"> 
-        <!-- <label for="email">email:</label> -->
-        <input type="email" placeholder="email" id="email" v-model="email" required>
+    <img src="../assets/howcanai_logo.svg" alt="Logo" class="logo-image" />
+    <form @submit.prevent="submitForm">
+      <div class="input-container">
+        <input class="input-field" type="text" id="username" v-model="username" placeholder="username" required>
       </div>
       <div class="input-container">
-        <!-- <label for="password">password:</label> -->
-        <input class="input-container" type="password" placeholder="password" id="password" v-model="password" required>
+        <input class="input-field" type="email" id="email" v-model="email" placeholder="email" required>
       </div>
       <div class="input-container">
-        <!-- <label for="confirm_password">confirm password:</label> -->
-        <input type="password" placeholder="check your password" id="confirm_password" v-model="confirm_password" required>
+        <input class="input-field" type="password" id="password" v-model="password" placeholder="password" required>
       </div>
-      <div style="bottom:0;">
       <div class="input-container">
-        <button type="submit">submit</button>
+        <input class="input-field" type="password" id="confirm_password" v-model="confirm_password"
+          placeholder="check your password" required>
       </div>
+      <div class="submit-container">
+        <button class="signup-button" type="submit">submit</button>
       </div>
-
     </form>
   </div>
 </template>
@@ -46,8 +37,8 @@ export default {
   methods: {
     submitForm() {
       if (this.password !== this.confirm_password) {
-          alert('비밀번호가 일치하지 않습니다.');
-          return;
+        alert('비밀번호가 일치하지 않습니다.');
+        return;
       }
       // 가입 폼 데이터 만들기
       const formData = {
@@ -56,14 +47,14 @@ export default {
         confirm_password: this.confirm_password,
         email: this.email
       };
-      
+
       // 백엔드로 데이터 전송
       // 가입처리 URL로 POST 요청
       axios.post('/user/create', formData)
         .then(response => {
           // 요청이 성공한 경우 처리 로직 작성
           console.log(response.data);
-          
+
           // 가입 처리 완료 후 리셋
           this.username = '';
           this.email = '';
@@ -81,12 +72,16 @@ export default {
 
 <style>
 .signup-container {
-  display: flex; flex-direction: column;
-  width: 680px;
-  height: 480px;
-  border-radius: 16px;
-  background: #242424;
+  width: 80%;
+  max-width: 680px;
+  min-height: 480px;
+  flex-shrink: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0 auto;
 }
+
 .input-container {
   display: flex;
   padding: 6px;
@@ -96,6 +91,7 @@ export default {
   /* border-radius: 17px; */
   /* background: #464646; */
 }
+
 .input-container input {
   /* width: 580px;
   height: 48px;
@@ -130,12 +126,14 @@ export default {
   outline: none;
   opacity: 0.7;
 }
+
 .input-container input:focus {
   color: white;
   opacity: 1;
   transition: 300ms;
 
 }
+
 .input-container input::placeholder {
   color: #979797;
   font-size: 14px;
@@ -144,6 +142,7 @@ export default {
   font-weight: 400;
   line-height: normal;
 }
+
 .input-container button {
   /* width: 580px;
   height: 48px;
@@ -179,4 +178,29 @@ export default {
   opacity: 0.7;
 }
 
-</style>
+.signup-button {
+  background-color: black;
+  color: darkgray;
+  border: 0;
+  border-radius: 5px;
+  padding: 0.5rem 1rem;
+  font-size: 1rem;
+  cursor: pointer;
+  font-family: Montserrat;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 1.5;
+  letter-spacing: 0.5px;
+}
+
+.signup-button:hover {
+  transform: translateY(-2px);
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.25);
+  transition: transform 0.3s ease-in-out, box-shadow 0.3s ease-in-out;
+}
+
+.submit-container {
+  display: flex;
+  justify-content: right;
+  width: 100%;
+}</style>
