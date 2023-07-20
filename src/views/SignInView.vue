721d521807c69d942a12eba79339dd4f8c746bee
gangjoohyeong <bles@kakao.com>
2023-07-20 18:21:23 +0900

    update: sign in style

--- src/views/SignInView.vue
+++ src/views/SignInView.vue
@@ -1,18 +1,13 @@
 <template>
   <div class="signin-container">
-    <h1>로그인</h1>
+    <img src="../assets/howcanai_logo.svg" alt="Logo" class="logo-image" />
     <form @submit.prevent="submitForm">
-      <div class="input-container">
-        <label for="username">유저 이름:</label>
-        <input class="input-field" type="text" id="username" v-model="username" required>
-      </div>
-      <div class="input-container">
-        <label for="username">비밀번호:</label>
-        <input class="input-field" type="password" id="password" v-model="password" required>
-      </div>
-
+      <div class="input-container"><input class="input-field" type="text" id="username" v-model="username"
+          placeholder="username" required> </div>
+      <div class="input-container"><input class="input-field" type="password" id="password" v-model="password"
+          placeholder="password" required> </div>
       <div class="submit-container">
-        <button class="signin-button" type="submit">로그인</button>
+        <button class="signin-button" type="submit">Sign in</button>
         <!-- 로그아웃 테스트 용 -->
         <!-- <button class="signout-button" type="button" @click="submitLogout">로그아웃</button> -->
       </div>
@@ -47,29 +42,80 @@ export default {
         // TODO: 이동 전에 chatList fetch 해오기 
         // this.resetChatList();
         // await this.fetchChatList();
-        console.log('chatList',this.$store.state.chatList);
+        console.log('chatList', this.$store.state.chatList);
         this.$router.push('/chat');
         console.log('chatList', this.$store.state.chatList);
       } catch (error) {
         alert(error.message);
       }
     },
-    // async submitLogout() {
-    //   try {
-    //     await this.logout();
-    //     alert('로그아웃 성공!');
-    //   } catch (error) {
-    //     alert('로그아웃 실패' + error.message);
-    //   }
-    // }
+    async submitLogout() {
+      try {
+        await this.logout();
+        alert('로그아웃 성공!');
+      } catch (error) {
+        alert('로그아웃 실패' + error.message);
+      }
+    }
   }
 }
 </script>
 
-<style>
+<style> 
   .signin-container {
-    width: 680px;
-    height: 480px;
+    width: 80%;
+    max-width: 680px;
+    min-height: 480px;
     flex-shrink: 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 0 auto;
   }
-</style>
+
+  .logo-image {
+    margin-top: 2rem;
+    width: 70%;
+    max-width: 300px;
+    margin-bottom: 2rem;
+  }
+
+  .input-container {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    width: 100%;
+  }
+
+  .input-field {
+    width: 100%;
+    padding: 0.5rem;
+    margin-top: 0.1rem;
+    border: 2px solid purple;
+    border-radius: 5px;
+    font-size: 1rem;
+    outline: none;
+  }
+
+  .signin-button {
+    background-color: black;
+    color: darkgray;
+    border: 0;
+    border-radius: 5px;
+    padding: 0.5rem 1rem;
+    font-size: 1rem;
+    cursor: pointer;
+  }
+
+
+ .signin-button:hover {
+  transform: translateY(-2px);
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.25);
+  transition: transform 0.3s ease-in-out, box-shadow 0.3s ease-in-out;
+}
+
+ .submit-container {
+   display: flex;
+   justify-content: right;
+   width: 100%;
+ }</style>
